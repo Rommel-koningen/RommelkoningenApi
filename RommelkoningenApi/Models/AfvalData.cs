95a7a072e4f99d3f0ea4a366a2abff1a85bc168c
Lars Kerste <laj.kerste@student.avans.nl>
2025-06-10 19:34:39 +0200

    Added afvaldata and fotodata models

---
+++ RommelkoningenApi/Models/AfvalData.cs
@@ -0,0 +1,10 @@
+﻿namespace RommelkoningenApi.Models
+{
+    public class AfvalData
+    {
+        public Guid Afval_Id { get; set; }
+        public Guid Foto_Id { get; set; }
+        public string Afval_Type { get; set; }
+        public float Confidence { get; set; }
+    }
+}
---
+++ RommelkoningenApi/Models/FotoData.cs
@@ -0,0 +1,15 @@
+﻿namespace RommelkoningenApi.Models
+{
+    public class FotoData
+    {
+        public Guid Foto_Id { get; set; }
+        public DateTime Datum_En_Tijd { get; set; }
+        public string Camera_Naam { get; set; }
+        public float Longitude { get; set; }
+        public float Latitude { get; set; }
+        public string Postcode { get; set; }
+        public string windrichting { get; set; }
+        public int temperatuur { get; set; }
+        public string Weer_Omschrijving { get; set; }
+    }
+}
